ca4df868410f303e060db03b769c989cdfac2706
asksven <sven.knispel@gmail.com>
2017-02-18 20:16:10 +0100

    Detect and skip VM that are not running Stop unpatched running VMs

--- SecurityMonkey/getSecurityState.ps1
+++ SecurityMonkey/getSecurityState.ps1
@@ -14,13 +14,9 @@ if ( ($Tenant -eq $null) -or ($ClientID -eq $null) -or ($ClientSecret -eq $null)
 }
 
 
-$Token            = Invoke-RestMethod -Uri https://login.microsoftonline.com/$tenant/oauth2/token?api-version=1.0 -Method Post -Body @{"grant_type" = "client_credentials"; "resource" = "https://management.core.windows.net/"; "client_id" = $ClientID; "client_secret" = $ClientSecret}
-
-#Write-Host "Access-token: ". $Token.access_token
+$Token = Invoke-RestMethod -Uri https://login.microsoftonline.com/$tenant/oauth2/token?api-version=1.0 -Method Post -Body @{"grant_type" = "client_credentials"; "resource" = "https://management.core.windows.net/"; "client_id" = $ClientID; "client_secret" = $ClientSecret}
 
 $subscriptionArray = $subscriptions -split ','
-
-#Write-Host "Subscription URI: ", $subscriptionURI
 
 $header = @{ 'authorization'="Bearer $($Token.access_token)" }
 
@@ -80,7 +76,14 @@ foreach ($SubscriptionID in $subscriptionArray)
       $rebootPending = ($element.properties.patchscannerdata.rebootPendingSecurityState -ne "Healthy")
       $missingPatches = ($element.properties.patchscannerdata.missingPatchesSecurityState -ne "Healthy")
       $securityState = ($element.properties.patchscannerdata.securityState -ne "Healthy")
-      #$element.properties.patchscannerdata
+
+      # Parse lastReportTime
+      $template = 'yyyy-MM-dd'
+      $lastReportDate = $element.properties.patchscannerdata.lastReportTime.Split("T")[0]
+      $lastReportTime = [DateTime]::ParseExact($lastReportDate, $template, $null)
+      $timeSpan = [System.DateTime]::Now - [DateTime]$lastReportTime;
+      $age = $timeSpan.TotalDays
+
       Write-Host "    Patch Status: " -NoNewLine
       $color = 'green';
       $status = "OK";
@@ -89,14 +92,59 @@ foreach ($SubscriptionID in $subscriptionArray)
 
       Write-Host " | " -NoNewLine
 
-      if ($missingPatches) { $color='red'; $status = "FAIL"; } else {$color='green'; $status = "PASS"; }
-      Write-Host missing patches: $status -Foreground $color -NoNewLine
+      # We want to check if the VM is running
+      $resourceName = ($element.id -split("/providers/Microsoft.Security"))[0]
 
-      Write-Host " | " -NoNewLine
 
-      if ($securityState) { $color='red'; $status = "FAIL"; } else {$color='green'; $status = "PASS"; }
-      Write-Host security state: $status -Foreground $color -NoNewLine
+      # we query the VM status
+      # see https://docs.microsoft.com/en-us/rest/api/compute/virtualmachines/virtualmachines-get
+      $vmURI  = "https://management.azure.com" + $resourceName + '/InstanceView?api-version=2017-03-30'
+      #Write-Host "Checking VM: " $vmURI
 
+      $vmState = 'unknown'
+
+      try
+      {
+        $Response = Invoke-RestMethod -Uri $vmURI -Headers $header -ContentType 'application/x-www-form-urlencoded'
+        if ($Response.statuses[1].code -eq "PowerState/running") {$vmState = "running"} else {$vmState = "stopped"}
+      }
+      catch
+      {
+        Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ -Foreground 'red'
+        Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription -Foreground 'red'
+      }
+
+      # if the vm is stopped we do not report a violation
+      if ($vmState -ne "stopped")
+      {
+        if ($missingPatches) { $color='red'; $status = "FAIL"; } else {$color='green'; $status = "PASS"; }
+        Write-Host missing patches: $status -Foreground $color -NoNewLine
+
+        Write-Host " | " -NoNewLine
+
+        if ($securityState) { $color='red'; $status = "FAIL"; } else {$color='green'; $status = "PASS"; }
+        Write-Host security state: $status -Foreground $color -NoNewLine
+
+        if ($missingPatches)
+        {
+          # we need to take action and stop the VM
+          $vmStopURI  = "https://management.azure.com" + $resourceName + '/powerOff?api-version=2016-04-30-preview'
+          try
+          {
+            $Response = Invoke-RestMethod -Method Post -Uri $vmStopURI -Headers $header -ContentType 'application/x-www-form-urlencoded'
+            Write-Host ">>> Action taken: VM was stopped!" -Foreground 'red'
+          }
+          catch
+          {
+            Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ -Foreground 'red'
+            Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription -Foreground 'red'
+          }
+        }
+      }
+      else
+      {
+        Write-Host "missing patches: VM state is $($vmState). Ignoring" -Foreground 'yellow' -NoNewLine
+      }
       Write-Host
     }
 
